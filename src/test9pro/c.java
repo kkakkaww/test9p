0a57cbb5536f89ff5f24a57ecaf1007fb7329d0f
kkakkaww <1715575996@qq.com>
2021-11-19 15:56:12 +0800

    9.5

---
+++ src/test9pro/c.java
@@ -0,0 +1,13 @@
+package test9pro;
+import java.util.GregorianCalendar;
+public class c {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GregorianCalendar t = new GregorianCalendar();
+		
+		t.setTimeInMillis(1234567898765L);
+		System.out.println(t.getTime());
+	}
+
+}
